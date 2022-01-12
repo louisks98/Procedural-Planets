5ca785ca83695e67c680336c4080f9eae6bd647d
louis <louis.christophe.poulin@gmail.com>
2022-01-12 16:22:50 -0500

    Shader class

---
+++ Procedural_Planets/src/shader.cpp
@@ -0,0 +1,101 @@
+#include "shader.h"
+
+using namespace std;
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+	string vstr = readFile(vertexPath);
+	const char* vertexCode = vstr.c_str();
+
+	string fstr = readFile(fragmentPath);
+	const char* fragCode = fstr.c_str();
+
+	compile(vertexCode, fragCode);
+}
+
+void Shader::use()
+{
+	glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string& name, bool value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setInt(const std::string& name, int value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string& name, float value) const
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+string Shader::readFile(const char* path)
+{
+	string code;
+	ifstream file;
+
+	file.exceptions(ifstream::failbit | ifstream::badbit);
+	try
+	{
+		file.open(path);
+		stringstream stream;
+		stream << file.rdbuf();
+
+		file.close();
+		return stream.str();
+	}
+	catch (exception e)
+	{
+		cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << endl;
+		cout << e.what() << endl;
+	}
+}
+
+void Shader::compile(const char* vertCode, const char* fragCode)
+{
+	unsigned int vertex;
+	unsigned int fragment;
+	int success;
+	char infoLog[512];
+
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vertCode, NULL);
+	glCompileShader(vertex);
+
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fragCode, NULL);
+	glCompileShader(fragment);
+
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	glLinkProgram(ID);
+
+	glGetProgramiv(ID, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(ID, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
---
+++ Procedural_Planets/src/shader.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+class Shader
+{
+public:
+    unsigned int ID;
+
+    Shader(const char* vertexPath, const char* fragmentPath);
+
+    void use();
+    void setBool(const std::string& name, bool value) const;
+    void setInt(const std::string& name, int value) const;
+    void setFloat(const std::string& name, float value) const;
+
+private:
+    std::string readFile(const char* path);
+    void compile(const char* vertCode, const char* fragCode);
+};
